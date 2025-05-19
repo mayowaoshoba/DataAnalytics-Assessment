3d7da21675e8615ced5606c423434807b3811fa3
mayowaoshoba <102654550+mayowaoshoba@users.noreply.github.com>
2025-05-19 10:36:01 +0100

    Update Assessment_Q1.sql

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -4,13 +4,13 @@ use adashi_staging;
 
 select
     a.id as owner_id,
-    concat(a.first_name, ' ', a.last_name) as name,
+    concat(a.first_name, ' ', a.last_name) as name, --to get full name, first_name and last_name was concatenated
     count(distinct b.id) as savings_count,
     count(distinct c.id) as investment_count,
-    sum(b.confirmed_amount)/100 as total_deposits
+    sum(b.confirmed_amount)/100 as total_deposits --converting from kobo to naira
 from
     users_customuser a
-join savings_savingsaccount b on b.owner_id = a.id and b.confirmed_amount > 0
+join savings_savingsaccount b on b.owner_id = a.id and b.confirmed_amount > 0 
 join plans_plan c on c.owner_id = a.id and c.is_a_fund = 1 and c.amount > 0
 group by
     a.id, a.first_name, a.last_name
